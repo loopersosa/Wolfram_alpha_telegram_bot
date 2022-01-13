04467bfa8a61ad42d1c576e0cd06be0852658076
loopersosa <87327421+loopersosa@users.noreply.github.com>
2022-01-13 22:06:04 +0330

    Wolfram-alpha-telegram-bot
    This is the ready telegram bot which connects Wolfram alpha to the telegram.
    Ready and running on the host.

---
+++ wolfarmalpha.py
@@ -0,0 +1,112 @@
+import wolframalpha
+import telebot, emoji, time
+
+
+#********************************************************************************
+#********************************************************************************
+
+# importing token
+TOKEN = "5027141328:AAE8pwBiwhW1gkV47blNLQw7BrJ26e6BDUs"
+
+# initializing bot
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'Start'])
+def start(message):
+  name = message.from_user.first_name
+  TEXT =  " خوش اومدی رفیق متقلب من " +emoji.emojize(" :winking_face_with_tongue: ")
+  bot.send_message(chat_id=message.chat.id, text=TEXT )
+
+@bot.message_handler(commands=['help', 'Help'])
+def help(message):
+
+    TEXT = emoji.emojize(" :blue_circle: ")
+    TEXT += "به این نمونه ها نگاه کن تا دستت بیاد  \n\n"
+    TEXT += "برای اینکه جواب رو بصورت عکس داشته باشی میتونی آخر دستور "
+    TEXT +="\n+img"
+    TEXT += "اضافه کنی (آخرین نمونه رو ببین)"
+    bot.send_message(chat_id=message.chat.id, text=TEXT )
+    url_manual_image = "https://i.postimg.cc/pLvrnn8v/manual-wolfram-bot.jpg"
+    bot.send_photo(chat_id=message.chat.id, photo=url_manual_image)
+
+# *************************************************************************
+##***************************************************************************
+#****************************************************************************@bot.message_handler(content_types=['sticker'])
+def sticker_handler(message):
+	bot.reply_to(message, " جدا فکر کردی میتونی با استیکر فرستادن منو دست بنداری؟ ")
+
+@bot.message_handler(content_types=['photo', 'audio', 'video', 'voice', "document", 'video_note', 'location'])
+def PAV_handler(message):
+	bot.reply_to(message, " خب الان من با این چی کنم ")
+
+@bot.message_handler(content_types=['animation'])
+def gif_handler(message):
+    bot.reply_to(message, text="(@magatowski) گیف هاتو بفرست برای مهدی ")
+
+# *************************************************************************
+##***************************************************************************
+#****************************************************************************
+@bot.message_handler(content_types=['text'])
+def main_handler(message):
+	main(message)
+
+# add the sleep and restarting
+
+def main(message):
+    chat_ID = message.chat.id
+    message = message.text.lower()
+
+    image_or_not = 0
+    if "+img" in message:
+        message = message[:-4]
+        image_or_not = 1
+
+    question = message
+    # App id obtained by the above steps
+    app_id = 'RJLVLK-WVTGUVW4TL'
+
+    # Instance of wolf ram alpha
+    # client class
+    client = wolframalpha.Client(app_id)
+
+    # Stores the response from
+    # wolf ram alpha
+    res = client.query(question)
+
+    if image_or_not == 1:
+        for pod in res.pods:
+            for sub in pod.subpods:
+                img_url = sub.img.src
+                bot.send_photo(chat_id=chat_ID, photo=img_url)
+
+    else :
+        ans = next(res.results).text
+
+        if '=' in ans :
+            print(ans)
+            i = ans.find("=")
+            fst, snd = ans[0:i], ans[i+1:]
+            TEXT = emoji.emojize(" :red_circle: ") + fst + ' = \n\n' + emoji.emojize(" :green_circle: ") +snd
+            bot.send_message(chat_id=chat_ID, text=TEXT)
+
+        else :
+            TEXT = emoji.emojize(" :red_circle: ") + ans
+            bot.send_message(chat_id=chat_ID, text=TEXT)
+
+
+# ****************************************************************
+#*****************************************************************
+
+# ****************************************************************
+#*****************************************************************
+
+try:
+    bot.infinity_polling()
+except Exception:
+    time.sleep(15)
+
+
+# *************************************************************************
+##***************************************************************************
+#****************************************************************************
+
