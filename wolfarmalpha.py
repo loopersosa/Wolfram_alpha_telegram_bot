ee03962d72bce817cf665e473925076aa03bc552
loopersosa <87327421+loopersosa@users.noreply.github.com>
2022-01-19 19:24:00 +0330

    Update

--- wolfarmalpha.py
+++ wolfarmalpha.py
@@ -1,18 +1,20 @@
 import wolframalpha
-import telebot, emoji, time
+import telebot, emoji, time, jdatetime
+from pytz import timezone
 
 
 #********************************************************************************
 #********************************************************************************
 
 # importing token
-TOKEN = "5027141328:AAE8pwBiwhW1gkV47blNLQw7BrJ26e6BDUs"
+TOKEN = ""
 
 # initializing bot
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'Start'])
 def start(message):
+
   name = message.from_user.first_name
   TEXT =  " خوش اومدی رفیق متقلب من " +emoji.emojize(" :winking_face_with_tongue: ")
   bot.send_message(chat_id=message.chat.id, text=TEXT )
@@ -31,7 +33,9 @@ def help(message):
 
 # *************************************************************************
 ##***************************************************************************
-#****************************************************************************@bot.message_handler(content_types=['sticker'])
+#****************************************************************************
+
+@bot.message_handler(content_types=['sticker'])
 def sticker_handler(message):
 	bot.reply_to(message, " جدا فکر کردی میتونی با استیکر فرستادن منو دست بنداری؟ ")
 
@@ -53,46 +57,61 @@ def main_handler(message):
 # add the sleep and restarting
 
 def main(message):
+    allowed_users = ['Magatowski', 'busybusy22', 'ayrozz', 'mahdi_shahini', 'Morteza_24']
+
     chat_ID = message.chat.id
-    message = message.text.lower()
 
-    image_or_not = 0
-    if "+img" in message:
-        message = message[:-4]
-        image_or_not = 1
+    jdatetime.set_locale('fa_IR')
+    info_line =  " " + jdatetime.datetime.now(timezone("Asia/Tehran")).strftime("%c")
+    print("\n" + message.from_user.username, info_line)
 
-    question = message
-    # App id obtained by the above steps
-    app_id = 'RJLVLK-WVTGUVW4TL'
 
-    # Instance of wolf ram alpha
-    # client class
-    client = wolframalpha.Client(app_id)
+    if message.from_user.username != None and  message.from_user.username in allowed_users :
 
-    # Stores the response from
-    # wolf ram alpha
-    res = client.query(question)
+        message = message.text.lower()
 
-    if image_or_not == 1:
-        for pod in res.pods:
-            for sub in pod.subpods:
-                img_url = sub.img.src
-                bot.send_photo(chat_id=chat_ID, photo=img_url)
+        image_or_not = 0
+        if "+img" in message:
+            message = message[:-4]
+            image_or_not = 1
 
-    else :
-        ans = next(res.results).text
+        question = message
+        # App id obtained by the above steps
+        app_id = ''
 
-        if '=' in ans :
-            print(ans)
-            i = ans.find("=")
-            fst, snd = ans[0:i], ans[i+1:]
-            TEXT = emoji.emojize(" :red_circle: ") + fst + ' = \n\n' + emoji.emojize(" :green_circle: ") +snd
-            bot.send_message(chat_id=chat_ID, text=TEXT)
+        # Instance of wolf ram alpha
+        # client class
+        client = wolframalpha.Client(app_id)
+
+        # Stores the response from
+        # wolf ram alpha
+        res = client.query(question)
+
+        if image_or_not == 1:
+            for pod in res.pods:
+                for sub in pod.subpods:
+                    img_url = sub.img.src
+                    bot.send_photo(chat_id=chat_ID, photo=img_url)
 
         else :
-            TEXT = emoji.emojize(" :red_circle: ") + ans
-            bot.send_message(chat_id=chat_ID, text=TEXT)
+            ans = next(res.results).text
 
+            if '=' in ans :
+                print(ans)
+                i = ans.find("=")
+                fst, snd = ans[0:i], ans[i+1:]
+                TEXT = emoji.emojize(" :red_circle: ") + fst + ' = \n\n' + emoji.emojize(" :green_circle: ") +snd
+                bot.send_message(chat_id=chat_ID, text=TEXT)
+
+            else :
+                TEXT = emoji.emojize(" :red_circle: ") + ans
+                bot.send_message(chat_id=chat_ID, text=TEXT)
+
+    else:
+        TEXT = 'دسترسی داده نمی شود. برای استفاده از ربات باید آیدی شما در لیست باشد'
+        TEXT += '\n\n به مهدی بگو اضافه کنه آيدیتو'
+        TEXT +='\n  ** @magatowski ** '
+        bot.send_message(chat_id=chat_ID, text=TEXT)
 
 # ****************************************************************
 #*****************************************************************
